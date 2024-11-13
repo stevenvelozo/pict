4077414b738c1d94e12e03578ff77c2bd2553b20
Steven Velozo <steven@velozo.com>
2024-11-13 06:09:27 -0800

    Working to fix bugs with other browsers and webpack

--- source/Pict.js
+++ source/Pict.js
@@ -178,7 +178,8 @@ class Pict extends libFable
 	{
 		let tmpOptions = (typeof(pOptions) == 'object') ? pOptions : {};
 		let tmpViewHash = (typeof(pViewHash) == 'string') ? pViewHash : this.fable.getUUID();
-		let tmpViewPrototype = (typeof(pViewPrototype) == 'function') ? pViewPrototype : undefined;
+		// This is here to change how the value is managed by webpack
+		let tmpViewPrototype = pViewPrototype;
 
 		if (this.LogControlFlow)
 		{
