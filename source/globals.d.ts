82cd9bd3f541e58e24c4e052730cae800448bc95
Alex Decker <alex.decker@headlight.com>
2025-05-07 12:37:03 -0700

    Spaces -> tabs.

--- source/globals.d.ts
+++ source/globals.d.ts
@@ -1,11 +1,11 @@
 declare global {
-    export interface Window {
-        _Pict: import('./Pict.js');
-        PictApplicaton: import('pict-application');
-        jQuery: typeof import('jquery');
-    }
-    var Pict: typeof import('./Pict.js');
-    var _Pict: import('./Pict.js');
+	export interface Window {
+		_Pict: import('./Pict.js');
+		PictApplicaton: import('pict-application');
+		jQuery: typeof import('jquery');
+	}
+	var Pict: typeof import('./Pict.js');
+	var _Pict: import('./Pict.js');
 }
 
 export {}
